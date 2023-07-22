43ecc3699baf63cd24c6e2f6b70c39201fdea8dd
Joshua Garrison <joshuagarrison27@gmail.com>
2023-07-22 19:33:27 -0400

    Change how to get config

--- KubeSharpOperator/Pages/kube.cshtml.cs
+++ KubeSharpOperator/Pages/kube.cshtml.cs
@@ -1,5 +1,8 @@
 using k8s;
+using k8s.KubeConfigModels;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Text;
+using YamlDotNet.Serialization;
 
 namespace KubeSharpOperator.Pages
 {
@@ -17,14 +20,33 @@ namespace KubeSharpOperator.Pages
 
             try
             {
-                // Decode the base64 string and save it as a file at the base of the project
-                byte[] kubeConfigBytes = Convert.FromBase64String(kubeConfigBase64);
-                string kubeConfigFilePath = "kubeconfig.yaml"; // Change this to the desired file name and extension
-                System.IO.File.WriteAllBytes(kubeConfigFilePath, kubeConfigBytes);
+                //// Decode the base64 string and save it as a file at the base of the project
+                //byte[] kubeConfigBytes = Convert.FromBase64String(kubeConfigBase64);
+                //string kubeConfigFilePath = "kubeconfig.yaml"; // Change this to the desired file name and extension
+                //System.IO.File.WriteAllBytes(kubeConfigFilePath, kubeConfigBytes);
 
-                // Build the Kubernetes client configuration from the file
-                var config = KubernetesClientConfiguration.BuildConfigFromConfigFile(kubeConfigFilePath);
+                //// Build the Kubernetes client configuration from the file
+                //var config = KubernetesClientConfiguration.BuildConfigFromConfigFile(kubeConfigFilePath);
+                //var client = new Kubernetes(config);
+
+                // Decode the base64 string into bytes
+                byte[] kubeConfigBytes = Convert.FromBase64String(kubeConfigBase64);
+
+                // Convert the bytes to a UTF-8 encoded string (YAML content)
+                string kubeConfigYaml = Encoding.UTF8.GetString(kubeConfigBytes);
+
+                // Create an instance of the deserializer
+                var deserializer = new DeserializerBuilder().Build();
+
+                // Deserialize the kube config YAML into the K8SConfiguration object
+                var k8sConfig = deserializer.Deserialize<K8SConfiguration>(kubeConfigYaml);
+                var config = KubernetesClientConfiguration.BuildConfigFromConfigObject(k8sConfig);
                 var client = new Kubernetes(config);
+
+                // You can now work with the K8SConfiguration object
+                // For example, access the current context name:
+                string currentContext = k8sConfig.CurrentContext;
+                Console.WriteLine($"Current Context: {currentContext}");
 
                 var namespaces = client.ListNamespace();
                 var itemList = new List<string>();
